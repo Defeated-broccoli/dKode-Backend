a1693e273cc7378a90500a37f10cb9977959ee21
Defeated-broccoli <piotrkwietniewski12@gmail.com>
2023-10-14 14:34:38 +0200

    Item now uses decimal instead of double for price.

--- BackEndPracticalTest/FactoryAPI/Models/Item.cs
+++ BackEndPracticalTest/FactoryAPI/Models/Item.cs
@@ -5,6 +5,6 @@
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
     }
 }
--- BackEndPracticalTest/FactoryDatabase/Tables/Items.sql
+++ BackEndPracticalTest/FactoryDatabase/Tables/Items.sql
@@ -1,7 +1,8 @@
-﻿CREATE TABLE [dbo].[Items]
-(
-	[ID] INT IDENTITY PRIMARY KEY, 
-    [Name] VARCHAR(50) NOT NULL, 
-    [Description] VARCHAR(100) NOT NULL, 
-    [Price] FLOAT NOT NULL
-)
+﻿CREATE TABLE [dbo].[Items] (
+    [ID]          INT             IDENTITY (1, 1) NOT NULL,
+    [Name]        VARCHAR (50)    NOT NULL,
+    [Description] VARCHAR (100)   NOT NULL,
+    [Price]       DECIMAL (18, 2) NOT NULL
+);
+
+
